b91b0ac616ad713f4e89fb76f297ea50f30e2393
Jessica <jkee007@e.ntu.edu.sg>
2019-10-16 18:01:19 +0800

    commit update to quiz logic

--- WorldOfSoftware/Scenes/Quiz/Question.gd
+++ WorldOfSoftware/Scenes/Quiz/Question.gd
@@ -12,23 +12,35 @@ onready var option2 = get_node(option2Path)
 onready var option3 = get_node(option3Path)
 onready var option4 = get_node(option4Path)
 
+#sample qns
+#FORMAT: {questionText, option1, option2, option3, option4, answerOption}
+var qn1 = ["Question 1 Text","Q1 Opt1", "Q1 Opt2","Q1 Opt1","Q1 Opt3", 1] 
+var qn2 = ["Question 2 Text","Q2 Opt1", "Q2 Opt2","Q2 Opt1","Q2 Opt3", 1] 
+var qn3 = ["Question 3 Text","Q3 Opt1", "Q3 Opt2","Q3 Opt1","Q3 Opt3", 1] 
+var qnsArr = [qn1,qn2,qn3]
+
 var score = 0
-var answer = 2 #set answer based on database
+var answer = 0 #set answer based on database
 var options = 4 #get number of options based on database
+signal correctResponse
 
 func _ready():
-	set_question()
+	for i in range(0, qnsArr.size()):
+		set_question(qnsArr[i][0],qnsArr[i][1],qnsArr[i][2],qnsArr[i][3],qnsArr[i][4],qnsArr[i][5])
+		yield(self,"correctResponse")
+	get_tree().quit() # will quit game for now
 
-func set_question():
-	questionText.set_text("Question 123") #set question based on database
-	option1.set_text("Option 1") #set options based on database
-	option2.set_text("Option 2")
+func set_question(qnText,opt1,opt2,opt3,opt4,ans):
+	answer = ans
+	questionText.set_text(qnText) #set question based on database
+	option1.set_text(opt1) #set options based on database
+	option2.set_text(opt2)
 	if(options == 4):
-		option3.set_text("Option 3") #set null if only 2 options needed
-		option4.set_text("Option 4")#set null if only 2 options needed
+		option3.set_text(opt3) 
+		option4.set_text(opt4) 
 	else:
-		option3.hide()
-		option4.hide()
+		option3.hide() #set null if only 2 options needed
+		option4.hide() #set null if only 2 options needed
 
 func _input(event):
 	var selected = 0
@@ -51,6 +63,6 @@ func checkAns(selected, answer):
 		score = score + 1;
 		print("Correct! Your score is ")
 		print(score)
+		emit_signal("correctResponse")
 	else:
-		print("Wrong! Your score is ")
-		print(score)
+		print("Wrong! Please try again")
