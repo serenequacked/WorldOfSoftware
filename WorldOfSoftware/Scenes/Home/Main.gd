d6a2910edc892d4d014971766786ec1ef1f10771
Aung <aungheinhtoo@outlook.com>
2019-10-22 18:38:16 +0800

    Add files

--- WorldOfSoftware/Scenes/Home/Main.gd
+++ WorldOfSoftware/Scenes/Home/Main.gd
@@ -2,8 +2,8 @@ extends Control
 
 
 func _on_RegisterButton_pressed() -> void:
-	get_tree().change_scene("res://interface/register/Register.tscn")
+	get_tree().change_scene("res://Scenes/Register/Register.tscn")
 
 
 func _on_LoginButton_pressed() -> void:
-	get_tree().change_scene("res://interface/login/Login.tscn")
+	get_tree().change_scene("res://Scenes/Login/Login.tscn")
